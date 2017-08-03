2f00bb008a64807142f9bad07433eebaf7cf1f35
kaka8686 <smilesd8686@gmail.com>
2017-08-03 16:01:47 +0800

    内容详情界面

--- app/src/main/java/com/tufuzk/materialdemo/FruitAdapter.java
+++ app/src/main/java/com/tufuzk/materialdemo/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.tufuzk.materialdemo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -45,7 +46,19 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
             context = parent.getContext();
         }
         View view = LayoutInflater.from(context).inflate(R.layout.fruit_item, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = fruitList.get(position);
+                Intent intent = new Intent(context, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getFruitName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getFruitImageId());
+                context.startActivity(intent);
+            }
+        });
+        return holder;
     }
 
     @Override
